82cbee85c0cfa66f47aeda126d877659cc3b08b3
Sébastien Bellon <seb.bellon@protonmail.com>
2022-06-26 12:58:03 +0200

    ♻️ update import path

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,12 +1,12 @@
-import 'package:ecommerce_app/src/features/account/account_screen.dart';
-import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
-import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
-import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
-import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../features/products_list/products_list_screen.dart';
+import '../features/account/account_screen.dart';
+import '../features/orders_list/orders_list_screen.dart';
+import '../features/shopping_cart/shopping_cart_screen.dart';
+import '../features/sign_in/email_password_sign_in_screen.dart';
+import '../features/sign_in/email_password_sign_in_state.dart';
 
 final goRouter = GoRouter(
   initialLocation: '/',
