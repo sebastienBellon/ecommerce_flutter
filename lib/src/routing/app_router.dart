8ad66caddf145c31321da4333fbb778eaa618c59
Sébastien Bellon <seb.bellon@protonmail.com>
2022-06-26 12:03:54 +0200

    ♻️ add a pageBuilder to our route to specifiy fullscreenDialog property

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../features/products_list/products_list_screen.dart';
@@ -13,7 +14,11 @@ final goRouter = GoRouter(
       routes: [
         GoRoute(
           path: 'cart',
-          builder: (context, state) => const ShoppingCartScreen(),
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const ShoppingCartScreen(),
+          ),
         ),
       ],
     ),
