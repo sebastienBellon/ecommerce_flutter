ec8f7adb8d934f4adab61ee16203f9c51fd2d5c9
Sébastien Bellon <seb.bellon@protonmail.com>
2022-06-26 11:54:31 +0200

    ♻️ convert route to card to new router

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
 import 'package:go_router/go_router.dart';
 
 import '../features/products_list/products_list_screen.dart';
@@ -9,6 +10,10 @@ final goRouter = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => const ProductsListScreen(),
+    ),
+    GoRoute(
+      path: '/cart',
+      builder: (context, state) => const ShoppingCartScreen(),
     ),
   ],
   // turn off the # in the URLs on the web
