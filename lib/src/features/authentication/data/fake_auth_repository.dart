cc8bca9ff65366b243200bd7d3b7d8666893d902
Sébastien bellon <sebastien.bellon@nagra.com>
2022-06-29 13:29:21 +0200

    ✨ add abstract class to allow the implementation of the future backend authentication using firebase

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,19 +1,62 @@
 import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FakeAuthRepository {
+abstract class AuthRepository {
+  Stream<AppUser?> authStateChanges();
+  AppUser? get currentUser;
+  Future<void> signInWithEmailAndPassword(String email, String password);
+  Future<void> createUserWithEmailAndPassword(String email, String password);
+  Future<void> signOut();
+}
+
+class FirebaseAuthRepository implements AuthRepository {
+  @override
+  Stream<AppUser?> authStateChanges() {
+    // TODO: implement authStateChanges
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> createUserWithEmailAndPassword(String email, String password) {
+    // TODO: implement createUserWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AppUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> signInWithEmailAndPassword(String email, String password) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}
+
+class FakeAuthRepository implements AuthRepository {
+  @override
   Stream<AppUser?> authStateChanges() => Stream.value(null); // TODO: update
 
+  @override
   AppUser? get currentUser => null; // TODO: update
 
+  @override
   Future<void> signInWithEmailAndPassword(String email, String password) async {
     // TODO: Implement
   }
+  @override
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
     // TODO: Implement
   }
 
+  @override
   Future<void> signOut() async {
     // TODO: Implement
   }
