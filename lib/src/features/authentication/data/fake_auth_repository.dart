d2433bb1c418db8f07ca8a4a6fe316adf7d20b21
Sébastien bellon <sebastien.bellon@nagra.com>
2022-06-29 15:45:23 +0200

    ✨ implementation of a controller using StateNotifier

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -2,51 +2,10 @@ import 'package:ecommerce_app/src/features/authentication/domain/app_user.dart';
 import 'package:ecommerce_app/src/utils/in_memory_store.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-abstract class AuthRepository {
-  Stream<AppUser?> authStateChanges();
-  AppUser? get currentUser;
-  Future<void> signInWithEmailAndPassword(String email, String password);
-  Future<void> createUserWithEmailAndPassword(String email, String password);
-  Future<void> signOut();
-}
-
-class FirebaseAuthRepository implements AuthRepository {
-  @override
-  Stream<AppUser?> authStateChanges() {
-    // TODO: implement authStateChanges
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> createUserWithEmailAndPassword(String email, String password) {
-    // TODO: implement createUserWithEmailAndPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  // TODO: implement currentUser
-  AppUser? get currentUser => throw UnimplementedError();
-
-  @override
-  Future<void> signInWithEmailAndPassword(String email, String password) {
-    // TODO: implement signInWithEmailAndPassword
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> signOut() {
-    // TODO: implement signOut
-    throw UnimplementedError();
-  }
-}
-
-class FakeAuthRepository implements AuthRepository {
+class FakeAuthRepository {
   final _authState = InMemoryStore<AppUser?>(null);
 
-  @override
   Stream<AppUser?> authStateChanges() => _authState.stream;
-
-  @override
   AppUser? get currentUser => _authState.value;
 
   void _createNewUser(String email) {
@@ -58,14 +17,12 @@ class FakeAuthRepository implements AuthRepository {
 
   void dispose() => _authState.close();
 
-  @override
   Future<void> signInWithEmailAndPassword(String email, String password) async {
     if (currentUser == null) {
       _createNewUser(email);
     }
   }
 
-  @override
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
     if (currentUser == null) {
@@ -73,13 +30,12 @@ class FakeAuthRepository implements AuthRepository {
     }
   }
 
-  @override
   Future<void> signOut() async {
     _authState.value = null;
   }
 }
 
-final authRepositoryProvider = Provider<AuthRepository>((ref) {
+final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
   final auth = FakeAuthRepository();
   // close the stream when not needed anymore
   ref.onDispose(() => auth.dispose());
